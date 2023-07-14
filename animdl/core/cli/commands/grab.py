5ab3ea06ffba489b51db3bb79d5002edfad3f138
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2023-07-15 00:16:09 +0530

    Update grab.py

--- animdl/core/cli/commands/grab.py
+++ animdl/core/cli/commands/grab.py
@@ -41,3 +41,23 @@ def animdl_grab(query, provider, **kwargs):
         stream_url = list(helpers.ensure_extraction(client, stream_url_caller))
         results.append({"episode": episode, "streams": stream_url})
     return results
+    
+def anim_grab(query, provider, **kwargs):
+    results = []
+    console = helpers.stream_handlers.get_console()
+    console.print(
+        "The content is outputted to [green]stdout[/] while these messages are outputted to [red]stderr[/]."
+    )
+
+    anime, provider = helpers.process_query(
+        client, query, console, auto_index=1, provider=provider
+    )
+
+    if not anime:
+        return
+    for stream_url_caller, episode in providers.get_appropriate(
+        client, anime.get("anime_url"), check=kwargs.get("range")
+    ):
+        stream_url = list(helpers.ensure_extraction(client, stream_url_caller))
+        results.append({"episode": episode, "streams": stream_url})
+    return results
