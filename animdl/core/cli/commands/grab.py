593ce8bbb28be906a8d101f65b971d0eca239e67
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2023-07-15 00:39:52 +0530

    Update grab.py

--- animdl/core/cli/commands/grab.py
+++ animdl/core/cli/commands/grab.py
@@ -8,26 +8,7 @@ from ...config import CHECK_FOR_UPDATES, DEFAULT_PROVIDER
 from .. import helpers
 from ..http_client import client
 
-def anim_grab(query, provider, **kwargs):
-    results = []
-    console = helpers.stream_handlers.get_console()
-    console.print(
-        "The content is outputted to [green]stdout[/] while these messages are outputted to [red]stderr[/]."
-    )
 
-    anime, provider = helpers.process_query(
-        client, query, console, auto_index=1, provider=provider
-    )
-
-    if not anime:
-        return
-    for stream_url_caller, episode in providers.get_appropriate(
-        client, anime.get("anime_url"), check=kwargs.get("range")
-    ):
-        stream_url = list(helpers.ensure_extraction(client, stream_url_caller))
-        results.append({"episode": episode, "streams": stream_url})
-    return results
-    
 @click.command(
     name="grab", help="Stream the stream links to the stdout stream for external usage."
 )
@@ -41,23 +22,27 @@ def anim_grab(query, provider, **kwargs):
 @helpers.decorators.banner_gift_wrapper(
     client, __core__, check_for_updates=CHECK_FOR_UPDATES
 )
-def animdl_grab(query, provider, **kwargs):
-    results = []
+def animdl_grab(query, index, **kwargs):
+
     console = helpers.stream_handlers.get_console()
     console.print(
         "The content is outputted to [green]stdout[/] while these messages are outputted to [red]stderr[/]."
     )
 
     anime, provider = helpers.process_query(
-        client, query, console, auto_index=1, provider=provider
+        client, query, console, auto_index=index, provider=DEFAULT_PROVIDER
     )
 
     if not anime:
         return
+
     for stream_url_caller, episode in providers.get_appropriate(
         client, anime.get("anime_url"), check=kwargs.get("range")
     ):
         stream_url = list(helpers.ensure_extraction(client, stream_url_caller))
-        results.append({"episode": episode, "streams": stream_url})
-    return results
-    
+        result = {
+            "anime": anime.get("name"),
+            "episode": episode,
+            "streams": stream_url
+        }
+        click.echo(json.dumps(result))
