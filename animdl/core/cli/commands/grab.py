3f85794fb2140eb45c1fb5ef088b133dd99672af
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2023-07-14 22:57:35 +0530

    Update grab.py

--- animdl/core/cli/commands/grab.py
+++ animdl/core/cli/commands/grab.py
@@ -1,4 +1,5 @@
 import json
+
 import click
 
 from ...__version__ import __core__
@@ -11,14 +12,18 @@ from ..http_client import client
 @click.command(
     name="grab", help="Stream the stream links to the stdout stream for external usage."
 )
-@click.option("--name", help="Name of the episode to retrieve stream links for")
+@helpers.decorators.content_fetch_options(
+    include_quality_options=False,
+    include_special_options=False,
+)
 @helpers.decorators.automatic_selection_options()
 @helpers.decorators.logging_options()
 @helpers.decorators.setup_loggers()
 @helpers.decorators.banner_gift_wrapper(
     client, __core__, check_for_updates=CHECK_FOR_UPDATES
 )
-def animdl_grab(query, index, name, **kwargs):
+def animdl_grab(query, index, **kwargs):
+
     console = helpers.stream_handlers.get_console()
     console.print(
         "The content is outputted to [green]stdout[/] while these messages are outputted to [red]stderr[/]."
@@ -31,21 +36,8 @@ def animdl_grab(query, index, name, **kwargs):
     if not anime:
         return
 
-    stream_urls = []
     for stream_url_caller, episode in providers.get_appropriate(
         client, anime.get("anime_url"), check=kwargs.get("range")
     ):
-        stream_urls.extend(helpers.ensure_extraction(client, stream_url_caller))
-
-    if name:
-        stream_urls = helpers.filter_streams_by_name(stream_urls, name)
-
-    click.echo(json.dumps({"streams": stream_urls}))
-
-
-def filter_streams_by_name(streams, name):
-    filtered_streams = []
-    for stream in streams:
-        if stream.get("name") == name:
-            filtered_streams.append(stream)
-    return filtered_streams
+        stream_url = list(helpers.ensure_extraction(client, stream_url_caller))
+        click.echo(json.dumps({"episode": episode, "streams": stream_url}))
