bb894165326d98a07792ea90a0fa227fdd4a43ad
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2023-07-16 17:43:40 +0530

    Update grab.py

--- animdl/core/cli/commands/grab.py
+++ animdl/core/cli/commands/grab.py
@@ -6,18 +6,39 @@ from ...config import CHECK_FOR_UPDATES, DEFAULT_PROVIDER
 from .. import helpers
 from ..http_client import client
 
+CACHE_DIR = "/tmp/anime_data_cache"
+
+def get_cache_filename(query, episode_number=None):
+    if episode_number is None:
+        return f"{CACHE_DIR}/{query}.json"
+    else:
+        return f"{CACHE_DIR}/{query}_ep{episode_number}.json"
+
 def animdl_grab(query, provider, episode_number=None):
     anime, selected_provider = helpers.process_query(
         client, query, None, auto_index=1, provider=provider
     )
     if not anime:
         return {}
-    
+
+    # Create cache directory if it doesn't exist
+    if not os.path.exists(CACHE_DIR):
+        os.makedirs(CACHE_DIR)
+
+    cache_filename = get_cache_filename(query, episode_number)
+
+    # Try to load data from cache
+    if os.path.exists(cache_filename):
+        with open(cache_filename, "r") as cache_file:
+            cached_data = json.load(cache_file)
+        if cached_data.get("anime_data"):
+            return cached_data
+
     episode_streams = []
     for stream_url_caller, episode in providers.get_appropriate(
         client, anime.get("anime_url")
     ):
-        if episode_number is None or episode == episode_number:
+        if episode_number is None or (episode_number is not None and episode == episode_number):
             stream_url = list(helpers.ensure_extraction(client, stream_url_caller))
             try:
                 title = anime.get("name")
@@ -27,4 +48,9 @@ def animdl_grab(query, provider, episode_number=None):
             episode_streams.append(episode_data)
 
     data_to_cache = {"anime_data": episode_streams}
+
+    # Save data to cache for future use
+    with open(cache_filename, "w") as cache_file:
+        json.dump(data_to_cache, cache_file)
+
     return data_to_cache
